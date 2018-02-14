9eb0f5debcd6371e982a1eed8354c435b899a3e3
Mircea Mihalea <mircea@mihalea.ro>
2018-02-14 15:39:00 +0000

    Create nextcloud_talk.js
    Add user scripts that changes the group call interface for the new Nextcloud Talk, copying the skype layout. Screen sharing button is disabled as I don't use that feature and the styling changes have broken it, and I can't be bothered to fix it.

---
+++ nextcloud_talk.js
@@ -0,0 +1,44 @@
+// ==UserScript==
+// @name     Nextcloud Talk - Better call layout
+// @description Better nextcloud talk user interface for group calling, copying skype
+// @version  1.2
+// @license MIT
+// @author Mircea Mihalea
+// @namespace ro.mihalea.talk
+// @grant    none
+// @require       http://code.jquery.com/jquery-3.3.1.min.js
+// @include  https://ds.mihalea.ro/call/*
+// ==/UserScript==
+
+$(document).ready(function() {
+  $(".videoContainer-dummy").css("display","none");
+  
+  unsafeWindow.OCA.SpreedMe.speakers.switchVideoToId = function (id) { return; };
+  
+  setInterval(function() {
+    $(".videoContainer").filter(function() {
+      return this.id.match("container_.+");
+    }).css({
+      "position": "relative",
+      "top": "50%",
+      "transform": "translateY(-50%)"
+    });
+
+    $("#localVideoContainer").css({
+      "position": "absolute",
+      "width": "10%",
+      "right": "15px",
+      "bottom": "15px",
+      "min-width": "200px"
+    });
+
+    $("#videos").css({
+      "align-items": "flex-start",
+      "background": "#000"
+    });
+
+    $(".videoContainer").css("padding", "0 1%");
+
+    $("#screensharing-button").css("display", "none");
+  }, 5000);
+});
