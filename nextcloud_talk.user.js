069651be07f0238c0ee23b25a4bf260341b3a1ef
Mircea Mihalea <mircea@mihalea.ro>
2018-02-14 16:11:38 +0000

    Update nextcloud_talk.user.js

--- nextcloud_talk.user.js
+++ nextcloud_talk.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name     Nextcloud Talk - Better call layout
 // @description Better nextcloud talk user interface for group calling, copying skype
-// @version  1.5.2
+// @version  1.6
 // @author Mircea Mihalea
 // @license MIT
 // @copyright 2018, mihalea (https://openuserjs.org/users/mihalea)
